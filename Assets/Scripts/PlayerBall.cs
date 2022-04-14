07fb8c623037cb0fecd6c654f6fa64486f69e44a
Doockan <avaz.yakubow@gmail.com>
2022-04-14 14:25:26 +0400

    download Project for 3 HW

---
+++ Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,15 @@
+﻿namespace Geekbrains
+{
+    public sealed class PlayerBall : Player
+    {
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        public void Test()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+ 
+namespace Geekbrains
+{
+    public class Test : MonoBehaviour
+    {
+        public delegate int TestDelegate(bool value);
+
+        public Func<int> FuncTest;
+
+        public Predicate<int> PredTest;
+
+        public TestDelegate DelegateField;
+        private void Start()
+        {
+            If(0);
+            DisplayShapesName(new Sphere());
+        }
+
+
+        public void Swap<T>(ref T a, ref T b)
+        {
+            (a, b) = (b, a);
+        }
+        
+        private interface IShape
+        {
+            void DisplayName();
+        }
+
+        private class Sphere : IShape
+        {
+            public void DisplayName()
+            {
+                Debug.Log("Sphere");
+            }
+        }
+
+        public class Triangle : IShape
+        {
+            public void DisplayName()
+            {
+                Debug.Log("Triangle");
+            }
+        }
+
+        private void DisplayShapesName(IShape shape)
+        {
+            shape.DisplayName();
+        }
+
+        private int Do()
+        {
+            return 0;
+        }
+
+        private bool If(int a)
+        {
+            if (a <= 0)
+            {
+               Debug.LogError("Value was zero");
+            }
+            return a > 0;
+        }
+
+        public class Observable
+        {
+            public Action Event;
+        }
+
+        public class Observer
+        {
+            public void Observe(){}
+        }
+    }
+}
